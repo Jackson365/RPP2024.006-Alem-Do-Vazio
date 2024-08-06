eb2430a179a5486a4ccb64681e4ec30c0e52436d
Jackson365 <Jackson.honorio@escolar.ifrn.edu.br>
2024-08-06 16:21:05 -0300

    Padrão Single e Camera

--- RPP AlemDoVazio/Assets/Scripts/GameController.cs
+++ RPP AlemDoVazio/Assets/Scripts/GameController.cs
@@ -4,20 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameController : MonoBehaviour
 {
-    public Text healthText;
-    
     public static GameController instance;
+    public Text healthText;
+
     // Start is called before the first frame update
     void Awake()
     {
-        instance = this;
+       if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }  
     }
     
     public void UpdateLives(int value)
     {
         healthText.text = "x " + value.ToString();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.P)){
+            SceneManager.LoadScene("Level2");
+        }
     }
 }
