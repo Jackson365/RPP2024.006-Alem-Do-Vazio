d38e9919b21ce0706f261ad071e2dfcda0ef15d3
Jackson365 <Jackson.honorio@escolar.ifrn.edu.br>
2024-08-31 17:45:41 -0300

    Ajeitando O EnemyPatroll

--- RPP AlemDoVazio/Assets/Scripts/EnemyS/EnemyPatroll.cs
+++ RPP AlemDoVazio/Assets/Scripts/EnemyS/EnemyPatroll.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class EnemyPatroll : MonoBehaviour
 {
@@ -10,65 +11,78 @@ public class EnemyPatroll : MonoBehaviour
     public int damage = 1;
     
     public Transform playerPos;
+    public Transform[] _position;
 
-    public float distance;
     public float speedEnemy;
+    public float waitingTime;
+    public float attackRange = 1f; // Distância máxima para atacar
 
-    private float timer;
-    public float walkTime;
-
-    private bool walkRight = true;
+    private int random;
+    private float time;
+    private bool isAttacking = false; // Variável para controlar se o inimigo está atacando
     
     public Rigidbody2D rig;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        rig.GetComponent<Rigidbody2D>();
+        random = Random.Range(0, _position.Length);
+        time = waitingTime;
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= walkTime)
+        if (!isAttacking)
         {
-            walkRight = !walkRight;
-            timer = 0f;
-        }
-
-        if (walkRight)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-            rig.velocity = Vector2.right * speedEnemy;
+            PatrollRandom();
         }
         else
         {
-            transform.eulerAngles = new Vector2(0, 0);
-            rig.velocity = Vector2.left * speedEnemy;
+            FollowPLayer();
         }
-        
+    }
+
+    public void PatrollRandom()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,_position[random].position, speedEnemy * Time.deltaTime);
+        float _dist = Vector2.Distance(transform.position, _position[random].position);
+
+        if (_dist <= 0.2f)
+        {
+            if (time <= 0)
+            {
+                random = Random.Range(0, _position.Length);
+                time = waitingTime; 
+            }
+            else
+            {
+                time -= Time.deltaTime;
+            }
+        }
     }
     
-    // Update is called once per frame
-    void Update()
+    private void FollowPLayer()
     {
-        distance = Vector2.Distance(transform.position, playerPos.position);
+        float distance = Vector2.Distance(transform.position, playerPos.position);
 
-        if (distance < 4)
+        if (distance <= attackRange)
         {
-            Seguir();
+            // Atacar o jogador
+            playerPos.GetComponent<PlayerController>().Damage(damage);
         }
-    }
-
-    private void Seguir()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speedEnemy * Time.deltaTime);
+        else if (distance > attackRange && distance < 4)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speedEnemy * Time.deltaTime);
+        }
+        else
+        {
+            isAttacking = false;
+        }
     }
     
     public void Damage (int vida)
     {
         health -= vida;
-        //anim.SetTrigger("hit");
 
         if(health <= 0)
         {
@@ -80,7 +94,7 @@ public class EnemyPatroll : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<PlayerController>().Damage(damage);
+            isAttacking = true;
         }
     }
 }
