144c76b161db5e13ea80c424e4a7574ce4083e44
Jackson365 <jackson.honorio@escolar.ifrn.edu.br>
2024-09-03 15:44:04 -0300

    Resolvendo Conflitos

--- RPP AlemDoVazio/Assets/Scripts/EnemyS/EnemyShoot.cs
+++ RPP AlemDoVazio/Assets/Scripts/EnemyS/EnemyShoot.cs
@@ -1,33 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyShoot : MonoBehaviour
 {
-    [Header("Atributtes")]
     public int health;
     public int damage = 1;
+    
+    public Transform playerPos;
+
     public float distance;
     public float speedEnemy;
-    
-    [Header("Components")]
-    public Transform playerPos;
-    public GameObject shoot;
-    public Transform firePoint;
-    public Rigidbody2D rig;
 
-    [Header("Others")]
     private float timer;
     public float walkTime;
+
     private bool walkRight = true;
+    
+    public GameObject shoot;
+    public Transform firePoint;
+    
     public float tempMax; 
     public float tempAtual; 
     
+    public Rigidbody2D rig;
+    public Animator animator;
+
     public PlayerController _playerController;
-   
+    
+    // Start is called before the first frame update
     void Start()
     {
-        rig.GetComponent<Rigidbody2D>();
+        rig = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -42,17 +48,20 @@ public class EnemyShoot : MonoBehaviour
 
         if (walkRight)
         {
-            transform.eulerAngles = new Vector2(0, 180);
+            transform.eulerAngles = new Vector2(0, 0);
             rig.velocity = Vector2.right * speedEnemy;
+            animator.SetInteger("Wait", 0);
         }
         else
         {
-            transform.eulerAngles = new Vector2(0, 0);
+            transform.eulerAngles = new Vector2(0, 180);
             rig.velocity = Vector2.left * speedEnemy;
+            animator.SetInteger("Wait", 0);
         }
         
     }
-   
+    
+    // Update is called once per frame
     void Update()
     {
         distance = Vector2.Distance(transform.position, playerPos.position);
@@ -65,19 +74,38 @@ public class EnemyShoot : MonoBehaviour
 
     private void FireShoot()
     {
+        StartCoroutine("Shoot");
+    }
+
+    IEnumerator Shoot()
+    {
         tempAtual -= Time.deltaTime; 
 
         if(tempAtual <= 0)
         {
-            Instantiate(shoot, firePoint.position, Quaternion.Euler(0f, 0f, 90f));
+            animator.SetInteger("Wait", 1);
+            GameObject shootEnemy = Instantiate(shoot, firePoint.position, Quaternion.Euler(0f, 0f, 90f));
             tempAtual = tempMax;
+            
+            if (transform.rotation.y == 0)
+            {
+                shootEnemy.GetComponent<ShootEnemy>().isRight = true;
+            }
+            if (transform.rotation.y == 180)
+            {
+                shootEnemy.GetComponent<ShootEnemy>().isRight = false;
+            }
+
+            yield return new WaitForSeconds(0.3f);
+            animator.SetInteger("Wait", 1);
         }
     }
     
     public void Damage (int vida)
     {
         health -= vida;
-        
+        animator.SetInteger("Wait", 2);
+
         if(health <= 0)
         {
             Destroy(gameObject);
